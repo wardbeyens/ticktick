6a5f3a006af4fef30cb02578039d17f16b1b7b41
Ward Beyens <wardbeyens99@gmail.com>
2023-02-08 15:18:17 +0100

    Update

---
+++ server/src/TickTick/TickTick.Api/Controllers/PersonsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TickTick.Models;
+
+namespace TickTick.Api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class PersonsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get() {
+            List<Person> people = new List<Person>();
+            people.Add(new Person ("John","Doe", "john@mail.com"));
+            people.Add(new Person("Kevin", "DeRudder", "kevin.derudder@gmail.com"));
+            return Ok(people);
+        }
+
+        [HttpGet("{id:Guid}")]
+        public IActionResult Get(Guid id)
+        {
+            //TODO: Haal een persoon op
+            Person person = new Person("Kevin", "DeRudder", "kevin.derudder@gmail.com");
+            return Ok(person);
+        }
+    }
+}
